76b065f80b812a1fd7ba3eaf9c4e2db7aa629495
Toshiaki Maki <makingx@gmail.com>
2024-12-09 19:56:41 +0900

    Make MockClock prototype scoped

--- src/test/java/am/ik/blog/MockConfig.java
+++ src/test/java/am/ik/blog/MockConfig.java
@@ -1,8 +1,10 @@
 package am.ik.blog;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 
 import java.time.Clock;
 import java.time.Instant;
@@ -14,6 +16,7 @@ public class MockConfig {
 
 	@Bean
 	@Primary
+	@Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	public Clock incrementalClock() {
 		Instant base = Instant.parse("2024-11-01T00:00:00Z");
 		AtomicInteger counter = new AtomicInteger(0);
