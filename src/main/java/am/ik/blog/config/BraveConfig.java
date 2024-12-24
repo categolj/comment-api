a623bf00c37a8ce29c9bbebf3e6dfbc42a216e5b
Toshiaki Maki <makingx@gmail.com>
2024-12-24 10:03:48 +0900

    Set InstrumentationScope

--- src/main/java/am/ik/blog/config/BraveConfig.java
+++ src/main/java/am/ik/blog/config/BraveConfig.java
@@ -1,10 +1,12 @@
 package am.ik.blog.config;
 
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.actuate.autoconfigure.opentelemetry.OpenTelemetryProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import zipkin2.reporter.Encoding;
+import zipkin2.reporter.otel.brave.InstrumentationScope;
 import zipkin2.reporter.otel.brave.OtlpProtoV1Encoder;
 
 @Configuration(proxyBeanMethods = false)
@@ -13,7 +15,10 @@ public class BraveConfig {
 
 	@Bean
 	public OtlpProtoV1Encoder otlpProtoV1Encoder(OpenTelemetryProperties properties) {
-		return OtlpProtoV1Encoder.newBuilder().resourceAttributes(properties.getResourceAttributes()).build();
+		return OtlpProtoV1Encoder.newBuilder()
+			.instrumentationScope(new InstrumentationScope("org.springframework.boot", SpringBootVersion.getVersion()))
+			.resourceAttributes(properties.getResourceAttributes())
+			.build();
 	}
 
 	@Bean
