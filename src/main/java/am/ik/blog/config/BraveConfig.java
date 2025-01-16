7a8d38c13e395162ecd4117916b8f3793c85af50
Toshiaki Maki <makingx@gmail.com>
2025-01-16 16:05:29 +0900

    Tweak

--- src/main/java/am/ik/blog/config/BraveConfig.java
+++ src/main/java/am/ik/blog/config/BraveConfig.java
@@ -1,5 +1,6 @@
 package am.ik.blog.config;
 
+import io.opentelemetry.proto.trace.v1.Span;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.actuate.autoconfigure.opentelemetry.OpenTelemetryProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import zipkin2.reporter.Encoding;
 import zipkin2.reporter.otel.brave.InstrumentationScope;
 import zipkin2.reporter.otel.brave.OtlpProtoV1Encoder;
+import zipkin2.reporter.otel.brave.TagToAttribute;
 import zipkin2.reporter.otel.brave.TagToAttributes;
 
 @Configuration(proxyBeanMethods = false)
@@ -21,7 +23,18 @@ public class BraveConfig {
 			.resourceAttributes(properties.getResourceAttributes())
 			.tagToAttributes(TagToAttributes.newBuilder()
 				.tagToAttribute("method", "http.request.method")
-				.tagToAttribute("uri", "http.route")
+				.tagToAttribute("uri", (builder, value) -> {
+					Span span = builder.build();
+					if (span.getKind() == Span.SpanKind.SPAN_KIND_SERVER) {
+						builder.addAttributes(TagToAttribute.stringAttribute("http.route", value));
+					}
+					else if (span.getKind() == Span.SpanKind.SPAN_KIND_CLIENT) {
+						builder.addAttributes(TagToAttribute.stringAttribute("url.template", value));
+					}
+					else {
+						builder.addAttributes(TagToAttribute.stringAttribute("uri", value));
+					}
+				})
 				.tagToAttribute("jdbc.query[0]", "db.query.text")
 				.tagToAttribute("jdbc.row-count", "db.response.returned_rows")
 				.tagToAttribute("Server Address", "server.address")
